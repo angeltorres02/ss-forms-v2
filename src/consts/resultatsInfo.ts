e463dc2f74f8dcb25849501e1b9da45ee47efcaa
Angel Torres <luis.vidalto@alumno.buap.mx>
2025-04-16 21:03:14 -0600

    Add result information constants and user data interface for improved data structure and clarity

---
+++ src/consts/resultatsInfo.ts
@@ -0,0 +1,14 @@
+export const SarcInfo = {
+  title: "SARC-F",
+  subtitle: "Resultados para la detección de sarcopenia",
+};
+
+export const NortonInfo = {
+  title: "Escala de Norton",
+  subtitle: "Resultados para el riesgo de úlceras por presión",
+};
+
+export const MnaInfo = {
+  title: "Mini-Nutritional Assessment-Short Form (MNA-SF)",
+  subtitle: "Resultados para la mini evaluación nutricional",
+};
---
+++ src/interface/userData.ts
@@ -0,0 +1,10 @@
+export interface UserData {
+  createdAt: string;
+  id: string;
+  medicoId: string;
+  pacienteId: string;
+  repuestas: object;
+  tipo: string;
+}
+
+exp;
