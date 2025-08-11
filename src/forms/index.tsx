ebb25df9377dd44ea4caf56d1f39ca204c7d5ce8
Angel Torres <luis.vidalto@alumno.buap.mx>
2025-08-11 01:22:46 -0600

    Relocate components

--- src/forms/index.tsx
+++
@@ -1,6 +0,0 @@
-export { Header } from "./components/Header";
-export { Form } from "./components/Form";
-export { Question } from "./components/Question";
-export { SubmitButton } from "./components/SubmitButton";
-export { Loading } from "./components/loading/Loading";
-export { CustomTooltip } from "./components/chart/CustomTooltip";
